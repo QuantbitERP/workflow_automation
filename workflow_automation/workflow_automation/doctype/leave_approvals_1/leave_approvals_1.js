9c6aa5b53d0fbc2a4502e47260c46e398ed747bc
kaushik-bansode <kaushik.bansode@erpdata.in>
2024-07-13 11:07:11 +0530

    first commit

---
+++ workflow_automation/workflow_automation/doctype/leave_approvals_1/leave_approvals_1.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, erpdata and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Leave Approvals-1', {
+	// refresh: function(frm) {
+
+	// }
+});
